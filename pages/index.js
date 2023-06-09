cb96eaa93fa91ed59c658a63298914c17608e267
andrxstorres <andrestorres9700@gmail.com>
2023-06-09 15:58:39 -0300

    Logo, NavBar sin funcionalidad y About asi nomas

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Navbar from "../components/navbar";
+import About from "@/components/about";
+import { indexStyles } from "../styles/_index.module.scss";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Navbar />
-      </div>
-    </>
+    <div className={indexStyles}>
+      <Navbar />
+      <About />
+    </div>
   );
 }
