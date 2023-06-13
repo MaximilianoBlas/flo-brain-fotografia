a9ed793fa80b6303efeccc48a19b24dd0d7ea0cf
andrxstorres <andrestorres9700@gmail.com>
2023-06-13 16:48:59 -0300

    update About

---
+++ components/galery.jsx
@@ -0,0 +1,9 @@
+import { galeryContainer, h2 } from "@/styles/_galery.module.scss";
+
+export default function Galery() {
+  return (
+    <div className={galeryContainer}>
+      <h2 className={h2}>Galery</h2>
+    </div>
+  );
+}
