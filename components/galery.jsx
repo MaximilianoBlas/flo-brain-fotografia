0c68878113aca9049dd0f23aef56df23daa40557
andrxstorres <andrestorres9700@gmail.com>
2023-06-16 15:20:06 -0300

    Galería funcional (con fotos de prueba).

--- components/galery.jsx
+++ components/galery.jsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-import { galeryContainer, h2, line, topDiv, bigPictureContainer, prevButton, nextButton, smallPictureContainer, smallPicture } from "@/styles/_galery.module.scss";
+import {
+  galeryContainer,
+  h2,
+  line,
+  topDiv,
+  bigPictureContainer,
+  prevButton,
+  nextButton,
+  smallPictureContainer,
+  miniPicSelect,
+  verP,
+  smallPicture,
+} from "@/styles/_galery.module.scss";
+
 import pic1 from "@/public/galerymedia/1.jpg";
 import pic2 from "@/public/galerymedia/2.jpg";
 import pic3 from "@/public/galerymedia/3.jpg";
@@ -9,34 +23,48 @@ import pic5 from "@/public/galerymedia/5.jpg";
 
 export default function Galery() {
   const galeryPics = [pic1, pic2, pic3, pic4, pic5];
+
+  let [currentImage, setCurrentImage] = useState(0);
+
+  function prevImage() {
+    currentImage > 0 ? setCurrentImage(currentImage - 1) : setCurrentImage((currentImage = 4));
+  }
+
+  function nextImage() {
+    currentImage < 4 ? setCurrentImage(currentImage + 1) : setCurrentImage((currentImage = 0));
+  }
+
+  function imageSelect(e) {
+    setCurrentImage(e.target.id);
+  }
+
   return (
     <div className={galeryContainer}>
-      <h2 className={h2}>Galery</h2>
+      <h2 className={h2}>Gallery</h2>
       <div className={topDiv}>
-        <button className={prevButton}>{"<"}</button>
-
-        <div className={bigPictureContainer}>{<Image src={pic1} fill={true} alt="Augmented image" />}</div>
-        <button className={nextButton}>{">"}</button>
+        <button className={prevButton} onClick={prevImage}>
+          {"<"}
+        </button>
+        <div className={bigPictureContainer}>{<Image src={galeryPics[currentImage]} fill={true} alt="Augmented gallery image" />}</div>
+        <button className={nextButton} onClick={nextImage}>
+          {">"}
+        </button>
       </div>
       <div className={smallPictureContainer}>
-        {/*galeryPics.map((pic) => {
+        {galeryPics.map((pic, index) => {
           return (
-            <div key={pic}>
-            <p>Ver</p>
-              <div className={smallPicture}>
-              <Image src={pic} fill />
+            <div key={index} id={index} className={miniPicSelect} onClick={imageSelect}>
+              <p id={index} className={verP}>
+                Ver
+              </p>
+              <div id={index} className={smallPicture}>
+                <Image id={index} src={pic} fill={true} placeholder="blur" alt="Miniature gallery image" />
               </div>
-              </div>
-              );
-        })*/}
+            </div>
+          );
+        })}
       </div>
       <div className={line}></div>
     </div>
   );
 }
-/*
- <div className={bigPicture}>
-   <Image src={pic1} fill={true} alt="Augmented size galery image" />
- </div>
- 
- */
