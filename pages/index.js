7ae1569df372a00ea2048218091849e941012eba
Maximiliano Blas Silva <silvamaxiblas@gmail.com>
2023-06-16 22:24:53 -0300

    contacto 1

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import Navbar from "./navbar";
+import Contacto from "./contacto";
 
 
 export default function Home() {
     return (
-        <>
-            <div>
-                inicio 
-                <Navbar />
+            <div >
+                <Contacto/>
             </div>
-        </>
     );
 }
