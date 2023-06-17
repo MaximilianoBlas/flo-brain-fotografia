4ba4c8d9ff7c16600969f3aa7e35e1c4e246bde7
Maximiliano Blas Silva <silvamaxiblas@gmail.com>
2023-06-17 17:43:17 -0300

    Correcciones Carousel, Services y Contacto

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,20 @@
 import Navbar from "../components/navbar";
 import About from "@/components/about";
 import Galery from "@/components/galery";
+import Carousel from "./carousel";
+import Services from "./services";
+import Contacto from "./contacto";
 import { indexStyles } from "../styles/_index.module.scss";
 
 export default function Home() {
   return (
     <div className={indexStyles}>
       <Navbar />
+      <Carousel/>
       <About />
+      <Services/>
       <Galery />
+      <Contacto/>
     </div>
   );
 }
