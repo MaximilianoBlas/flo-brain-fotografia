b0925e8677079734d20d83266fdd9d1d6389b1f9
Maximiliano Blas Silva <silvamaxiblas@gmail.com>
2023-06-22 22:41:11 -0300

    Correcciones para vista desktop

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,26 @@ import Galery from "@/components/galery";
 import Carousel from "./carousel";
 import Services from "./services";
 import Contacto from "./contacto";
-import Map from "./map"
-import { indexStyles } from "../styles/_index.module.scss";
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import LoaderMap from "./loaderMap";
+import { indexStyles, mapContactoContainer, mapContainer, contactoContainer } from "../styles/_index.module.scss";
 
 export default function Home() {
-        const { isLoaded } = useLoadScript({
-            googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-        });
 
-        if (!isLoaded) return <div>Loading...</div>;
   return (
       <div className={indexStyles}>
-          <Map/>
+          <Navbar />
+          <Carousel />
+          <About />
+          <Services />
+          <Galery />
+          <div className={mapContactoContainer}>
+            <div className={mapContainer}>
+              <LoaderMap />
+            </div>
+            <div className={contactoContainer}>
+              <Contacto />
+            </div>
+          </div>
       </div>
   );
 }
